e1ff58a36b367ead3339cfc892a6283536b12622
19041265 <19041265@DESKTOP-TUA3BPF>
2020-08-13 14:14:49 +0800

    Added the account class for the package

---
+++ C206_CaseStudy/src/Account.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19041265, 13 Aug 2020 2:03:39 pm
+ */
+
+public class Account {
+private String username;
+private String password;
+private String userRole;
+public Account(String username, String password, String userRole) {
+	this.username = username;
+	this.password = password;
+	this.userRole = userRole;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getUserRole() {
+	return userRole;
+}
+public void setUserRole(String userRole) {
+	this.userRole = userRole;
+}
+public String toString()
+{
+	String test = null;
+	return test;
+}
+
+
+}
--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
