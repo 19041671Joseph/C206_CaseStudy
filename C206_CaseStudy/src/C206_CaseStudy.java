5f457a6a6bcdb64edf28e2a3381020f007b32ee4
19041265 <19041265@DESKTOP-TUA3BPF>
2020-08-13 15:35:23 +0800

    Helper class and account methods added

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -6,4 +6,34 @@ public class C206_CaseStudy {
 ArrayList<Account> accountList=new ArrayList<>();
 	}
 
+	
+	public void addAccount(ArrayList<Account> aList)
+	{
+		String username=Helper.readString("Enter new username >");
+		String password=Helper.readString("Enter password >");
+		String userRole=Helper.readString("Enter the user role > ");
+		aList.add(new Account(username,password,userRole));
+	}
+	public void deleteAccount(ArrayList<Account> aList)
+	{
+		viewAllAccount(aList);
+		String username=Helper.readString("Enter account to delete");
+		for (int i=0;i<aList.size();i++)
+		{
+			if (username.equalsIgnoreCase(aList.get(i).getUsername()))
+			{
+				aList.remove(i);
+				System.out.println("User successfully removed");
+			}
+		}
+	}
+	public void viewAllAccount(ArrayList<Account> aList)
+	{
+		String display=String.format("%s %10s %10s", "Username","Password","Role");
+		for (Account a:aList)
+		{
+			display+=a.toString();
+		}
+		System.out.println(display);
+	}
 }
