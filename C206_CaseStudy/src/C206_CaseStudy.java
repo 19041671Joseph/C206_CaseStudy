a19303d78d95d7b77cca9a8eac708815e1f313f6
19041265 <19041265@DESKTOP-TUA3BPF>
2020-08-19 14:38:43 +0800

    Working on the test for the account

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,30 +1,60 @@
 import java.util.ArrayList;
 public class C206_CaseStudy {
-
+	private static String c="Customer";
+	private static String s="Stall Staff";
+	private static String ca="Canteen Admin";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 ArrayList<Account> accountList=new ArrayList<>();
+
 	}
 
 	
-	public void addAccount(ArrayList<Account> aList)
+	public static void addAccount(ArrayList<Account> aList)
 	{
 		String username=Helper.readString("Enter new username >");
 		String password=Helper.readString("Enter password >");
 		String userRole=Helper.readString("Enter the user role > ");
+		if (userRole.equalsIgnoreCase(c))
+		{
 		aList.add(new Account(username,password,userRole));
+		System.out.println("New user role has been added!");
+		}
+		else if (userRole.equalsIgnoreCase(s))
+		{
+			aList.add(new Account(username,password,userRole));
+			System.out.println("New user role has been added!");
+		}
+		else if (userRole.equalsIgnoreCase(ca))
+		{
+			aList.add(new Account(username,password,userRole));
+			System.out.println("New user role has been added!");
+		}
+		else
+		{
+			System.out.println("User role is unidentified");
+		}
+		
 	}
 	public void deleteAccount(ArrayList<Account> aList)
 	{
 		viewAllAccount(aList);
 		String username=Helper.readString("Enter account to delete");
+		boolean notFound=true;
 		for (int i=0;i<aList.size();i++)
 		{
 			if (username.equalsIgnoreCase(aList.get(i).getUsername()))
 			{
 				aList.remove(i);
 				System.out.println("User successfully removed");
+				notFound=false;
+				break;
 			}
+		}
+		if (notFound==true)
+		{
+			System.out.println("User cannot be found");
 		}
 	}
 	public void viewAllAccount(ArrayList<Account> aList)
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,17 +1,60 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
-
+	private Account customer1;
+	private Account customer2;
+	private Account stallStaff1;
+	private Account stallStaff2;
+	private Account canteenAdmin1;
+	private Account canteenAdmin2;
+	
+	private String c="Customer";
+	private String s="Stall Staff";
+	private String ca="Canteen Admin";
+	
+	private ArrayList <Account> accountList;
+	public C206_CaseStudyTest()
+	{
+		super();
+	}
 	@Before
 	public void setUp() throws Exception {
+		customer1=new Account("Liu","mk201920",c);
+		customer2=new Account("Chua","C1u6",c);
+		stallStaff1=new Account("Kwan yew Huat","KyH",s);
+		stallStaff2=new Account("Wee Swee Keat","WSKSG225",s);
+		canteenAdmin1=new Account("Koh Liam Cheng","$1n9ap0r3",ca);
+		canteenAdmin2=new Account("Lee Ching Chiong","Kua1a 1umpur",ca);
+		
+		accountList= new ArrayList<Account>();
 	}
 
 	@After
+	public void addAccountTest()
+	{
+		assertNotNull("Test there is a valid account list to add to",accountList);
+		
+		C206_CaseStudy.addAccount(accountList);
+		assertEquals("Test if arrayList size is 1?",1,accountList.size());
+		assertSame("Test of account is added as the 1st item of the list",customer1,accountList.get(0));
+		C206_CaseStudy.addAccount(accountList);
+	}
+	@After
 	public void tearDown() throws Exception {
+		customer1=null;
+		customer2=null;
+		stallStaff1=null;
+		stallStaff2=null;
+		canteenAdmin1=null;
+		canteenAdmin2=null;
+		
+		accountList=null;
 	}
 
 	@Test
